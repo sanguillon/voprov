ba7ce963a986f43364a78b7d26668d157953f636
jean-francois-sornay <jean-francois.sornay@etu.umontpellier.fr>
2020-06-16 18:37:58 +0200

    Adding a README.rst, a LICENSE and a setup file for deployment to the Python Package Index.

---
+++ README.rst
@@ -0,0 +1,21 @@
+Introduction
+============
+
+A library for IVOA Provenance Data Model supporting PROV-N, PROV-XML, PROV-JSON import/export
+
+* Free software: MIT license
+* Documentation: (incoming).
+
+Features
+--------
+
+* An implementation of the [IVOA Provenance Data Model](http://www.ivoa.net/documents/ProvenanceDM/) in Python.
+* Serialization and deserialization support: [PROV-N](http://www.w3.org/TR/prov-n/), [PROV-XML](http://www.w3.org/TR/prov-xml/>) and [PROV-JSON](http://www.w3.org/Submission/prov-json/).
+* Exporting VOPROV documents into various graphical formats (e.g. PDF, PNG, SVG).
+* Convert a VOPROV document to a [Prov Document](https://github.com/trungdong/prov).
+
+
+Uses
+----
+
+A short tutorial for using this package is in development.
---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    readme = f.read()
+
+requirements = [
+    'prov>=1.5.3',
+]
+
+test_requirements = [
+    'pydot>=1.2.0'
+]
+
+setup(
+    name='voprov',
+    version='1.0.0',
+    description='A library for IVOA Provenance Data Model supporting PROV-JSON, '
+                'PROV-XML and PROV-N',
+    long_description=readme,
+    author='Jean-Francois Sornay',
+    author_email='jean-francois.sornay@gmail.com',
+    url='https://github.com/sanguillon/voprov/',
+    packages=find_packages(where="voprov"),
+    package_dir={'': 'voprov'},
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require={
+        'dot': ['pydot>=1.2.0'],
+    },
+    license="MIT",
+    zip_safe=False,
+    keywords=[
+        'provenance', 'graph', 'model', 'VOPROV', 'provenance-dm', 'PROVENANCE-DM', 'PROV-JSON', 'JSON',
+        'PROV-XML', 'PROV-N'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: French',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+    tests_require=test_requirements,
+    python_requires='>=2',
+)
