5e50e2bb6bec226066f0f90d6dce6e2462c63a77
bparciany <benjamin.parciany@obspm.fr>
2023-04-26 10:01:40 +0200

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,15 @@
 #
 import os
 import sys
+import mock
+
 sys.path.insert(0, os.path.abspath('../voprov/'))
 
+MOCK_MODULES = ['voprov', 'voprov.models.constants', 'voprov.models.model', 'voprov.models.voprovConfigurations',
+                'voprov.models.voprovDescriptions', 'voprov.models.voprovRelations', 'voprov.serializers.provn',
+                'voprov.serializers.xml', 'voprov.visualization.dot', 'voprov.visualization.graph']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # -- Project information -----------------------------------------------------
 
